e438dbbb0b137d02a6d3d3f759ece1aeeee87774
Michael Kravchenko <rezkiy37@gmail.com>
2021-05-16 13:41:39 +0300

    implement it into Tabs;

--- src/components/Tabs/static.tsx
+++ src/components/Tabs/static.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { TTabs } from './types'
+import { Table } from '..'
 
 export const StaticTabs: TTabs = [
   {
     name: 'log',
     path: '/',
     label: 'tasks log',
-    component: () => <p>log</p>,
-    // TODO implement real component
+    component: Table,
   },
   {
     name: 'chart',
     path: '/chart',
     label: 'tasks chart',
-    component: () => <p>chart</p>,
+    component: Table,
     // TODO implement real component
   },
 ]
